5809882d9ada5b851ad516e5156eb2970c49a3b0
Christophe Michel <1050366+christopheml@users.noreply.github.com>
2023-12-13 11:34:06 +0100

    Jour 12 : nettoyage

--- src/main/kotlin/fr/christopheml/aoc2023/day12/HotSprings.kt
+++ src/main/kotlin/fr/christopheml/aoc2023/day12/HotSprings.kt
@@ -21,39 +21,39 @@ class HotSprings : Solution<Long>(12) {
 
     private val cache = HashMap<Pair<String, List<Int>>, Long>()
 
-    fun cacheAndReturn(records: String, groups: List<Int>, result: Long): Long {
+    private fun cacheAndReturn(records: String, groups: List<Int>, result: Long): Long {
         cache[Pair(records, groups)] = result
         return result
     }
 
-    fun arrangements(records: String, groups: List<Int>, depth: Int, log: String): Long {
+    fun arrangements(records: String, groups: List<Int>): Long {
         val cacheKey = Pair(records, groups)
         if (cache[cacheKey] != null) return cache[cacheKey]!!
-        println("  ".repeat(depth) + "arr('$records', $groups) [$log]")
 
-        // Petite optimisation (uniquement valide sur la fin)
-        if (records.count { it == '?' || it == '#' } < groups.sum() || records.length < minimumSizeOfRecords(groups)) { println("  ".repeat(depth) + "record too short"); return 0L }
+        if (records.count { it == '?' || it == '#' } < groups.sum() || records.length < minimumSizeOfRecords(groups))
+            return 0L
 
-        // Condition de retour
         if (groups.isEmpty()) return if (records.count { it == '#' } == 0) 1L else 0
 
-        return cacheAndReturn(records, groups, when (records[0]) {
-            '.' -> {
-                val skipTo = min(records.indexOfFirst { it != '.' }, records.length - 1)
-                arrangements(records.drop(skipTo), groups, depth + 1, "skip $skipTo")
+        return cacheAndReturn(
+            records, groups, when (records[0]) {
+                '.' -> {
+                    val skipTo = min(records.indexOfFirst { it != '.' }, records.length - 1)
+                    arrangements(records.drop(skipTo), groups)
+                }
+                '#' -> {
+                    if (canEat(records, groups[0])) arrangements(eat(records, groups[0]), groups.drop(1)) else 0L
+                }
+                else -> {
+                    arrangements(records.drop(1), groups) +
+                            if (canEat(records, groups[0])) arrangements(
+                                eat(records, groups[0]),
+                                groups.drop(1)
+                            ) else 0L
+                }
             }
-            '#' -> {
-                // tentative de match le groupe et de passer à la suite
-                if (canEat(records, groups[0])) arrangements(eat(records, groups[0]), groups.drop(1), depth + 1, "eat") else 0L
-            }
-            else -> {
-                arrangements(records.drop(1), groups, depth + 1, "skip") +
-                        if (canEat(records, groups[0])) arrangements(eat(records, groups[0]), groups.drop(1), depth + 1, "eat") else 0L
-            }
-        })
+        )
     }
-
-    fun arrangements(records: String, groups: List<Int>): Long = arrangements(records, groups, 0, "init")
 
     override fun partOne(input: Input): Long {
         return input.multi.asSequence().map {
